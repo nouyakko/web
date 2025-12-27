0cdf93c56775495351c1e416980e27b9c7313e3a
nouyakko <shimon.kasugai@gmail.com>
2025-12-27 16:34:02 +0900

    起動に成功

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,3 @@
-import { useEffect, useState } from "react"
-
-function App() {
-  const [message, setMessage] = useState("")
-
-  useEffect(() => {
-    fetch("http://localhost:8000/")
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-  }, [])
-
-  return (
-    <div>
-      <h1>React + FastAPI</h1>
-      <p>{message}</p>
-    </div>
-  )
+export default function App() {
+  return <h1>Hello Vite + React 👋</h1>
 }
-
-export default App
